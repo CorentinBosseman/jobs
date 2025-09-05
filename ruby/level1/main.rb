87401b7d283a41324ed104a125b2cbe62c3f5941
Corentin Bosseman <cbosseman@dexem.com>
2025-09-05 22:55:16 +0200

    Level 1

---
+++ ruby/level1/main.rb
@@ -0,0 +1,55 @@
+require "json"
+require "date"
+
+class Car
+  attr_reader :id, :price_per_day, :price_per_km
+
+  def initialize(attrs)
+    @id            = attrs["id"]
+    @price_per_day = attrs["price_per_day"]
+    @price_per_km  = attrs["price_per_km"]
+  end
+end
+
+class Rental
+  attr_reader :id, :car_id, :start_date, :end_date, :distance
+
+  def initialize(attrs)
+    @id         = attrs["id"]
+    @car_id     = attrs["car_id"]
+    @start_date = Date.parse(attrs["start_date"])
+    @end_date   = Date.parse(attrs["end_date"])
+    @distance   = attrs["distance"]
+  end
+
+  def duration_in_days
+    (@end_date - @start_date).to_i + 1
+  end
+
+  def total_price(car)
+    duration_price = duration_in_days * car.price_per_day
+    distance_price = @distance * car.price_per_km
+    duration_price + distance_price
+  end
+end
+
+input_path  = File.join(__dir__, "data", "input.json")
+output_path = File.join(__dir__, "data", "output.json")
+
+data = JSON.parse(File.read(input_path))
+cars = data["cars"].map { Car.new(_1) }
+rentals = data["rentals"].map { Rental.new(_1) }
+
+puts "#{cars.size} cars found, #{rentals.size} rentals found"
+
+cars_by_id = cars.to_h { |c| [c.id, c] }
+
+output = {
+  rentals: rentals.map do |rental|
+    car = cars_by_id.fetch(rental.car_id)
+    { id: rental.id, price: rental.total_price(car) }
+  end
+}
+
+File.write(output_path, JSON.pretty_generate(output))
+puts "Wrote #{output_path}"
