08bab25a1bb74052f708231d3b8704f0550466ac
Corentin Bosseman <cbosseman@dexem.com>
2025-09-10 22:18:54 +0200

    Refactor Level3

--- ruby/level3/test.rb
+++ ruby/level3/test.rb
@@ -1,10 +1,10 @@
 require "minitest/autorun"
 require "json"
-require_relative "../level1/main"
+require_relative "../level3/main"
 
-class Level1UnitTest < Minitest::Test
-  Car    = Level1::Car
-  Rental = Level1::Rental
+class Level3UnitTest < Minitest::Test
+  Car    = Level3::Car
+  Rental = Level3::Rental
 
   def car
     Car.new({ "id"=>1, "price_per_day"=>2000, "price_per_km"=>10 })
@@ -22,7 +22,12 @@ class Level1UnitTest < Minitest::Test
   end
 
   def test_total_price_three_days
-    assert_equal 7000, rental_three_days.total_price
+    assert_equal 6600, rental_three_days.total_price
+  end
+
+  def test_commission_breakdown_three_days
+    expected = { insurance_fee: 990, assistance_fee: 300, drivy_fee: 690 }
+    assert_equal expected, rental_three_days.commission
   end
 
   def test_output
